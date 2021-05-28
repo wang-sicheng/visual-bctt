d827b2c2643825f3c27ac106585520faa17b819e
levan <744915067@qq.com>
2021-05-28 21:48:33 +0800

    commit5-28-3

--- vue.config.js
+++ vue.config.js
@@ -24,7 +24,9 @@ module.exports = {
    * In most cases please use '/' !!!
    * Detail: https://cli.vuejs.org/config/#publicpath
    */
-  publicPath: '/',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/visual-bctt/'
+    : '/',
   outputDir: 'dist',
   assetsDir: 'static',
   lintOnSave: process.env.NODE_ENV === 'development',
