3932e0be40a0b8af4c7b9d516a09a197c3e9a36c
wang-sicheng <wangsicheng@pku.edu.cn>
2021-11-28 14:54:34 +0800

    将交易信息分成三个标签页：转账交易、发布合约和调用合约

--- src/views/getalltrans/index.vue
+++ src/views/getalltrans/index.vue
@@ -1,54 +1,111 @@
 <template>
   <div class="app-container">
-    <el-table
-      v-loading="listLoading"
-      :data="list"
-      element-loading-text="Loading"
-      border
-      highlight-current-row
-      max-height="800"
-    >
-<!--      <el-table-column align="center" width="400" label="交易哈希">-->
-<!--        <template slot-scope="scope">-->
-<!--          {{ scope.row.id }}-->
-<!--        </template>-->
-<!--      </el-table-column>-->
-      <el-table-column label="从" width="350" align="center">
-        <template slot-scope="scope">
-          <span>{{ scope.row.from }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column label="到" width="350" align="center">
-        <template slot-scope="scope">
-          <span>{{ scope.row.to }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column label="交易额" width="90" align="center">
-        <template v-if="scope.row.contract === ''" slot-scope="scope">
-          <span>{{ scope.row.value }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column align="center" label="智能合约" width="150">
-        <template slot-scope="scope">
-          <span>{{ scope.row.contract }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column align="center" label="方法" width="150">
-        <template v-if="scope.row.contract !== ''" slot-scope="scope">
-          <span>{{ scope.row.method }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column align="center" label="参数" width="200">
-        <template v-if="scope.row.contract !== ''" slot-scope="scope">
-          <span>{{ scope.row.args }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column align="center" prop="created_at" label="交易时间" width="200">
-        <template slot-scope="scope">
-          <span>{{ getTime(scope.row.timestamp) }}</span>
-        </template>
-      </el-table-column>
-    </el-table>
+    <el-tabs v-model="activeName" type="card" @tab-click="handleClick">
+      <el-tab-pane label="转账交易" name="first">
+        <el-table
+          v-loading="listLoading"
+          :data="transferTrans"
+          element-loading-text="Loading"
+          border
+          highlight-current-row
+          max-height="800"
+        >
+          <el-table-column label="从" width="350" align="center">
+            <template slot-scope="scope">
+              <span>{{ scope.row.from }}</span>
+            </template>
+          </el-table-column>
+          <el-table-column label="到" width="350" align="center">
+            <template slot-scope="scope">
+              <span>{{ scope.row.to }}</span>
+            </template>
+          </el-table-column>
+          <el-table-column label="交易额" width="90" align="center">
+            <template v-if="scope.row.contract === ''" slot-scope="scope">
+              <span>{{ scope.row.value }}</span>
+            </template>
+          </el-table-column>
+          <el-table-column align="center" prop="created_at" label="交易时间" width="200">
+            <template slot-scope="scope">
+              <span>{{ getTime(scope.row.timestamp) }}</span>
+            </template>
+          </el-table-column>
+        </el-table>
+      </el-tab-pane>
+
+      <el-tab-pane label="发布合约" name="second">
+        <el-table
+          v-loading="listLoading"
+          :data="postTrans"
+          element-loading-text="Loading"
+          border
+          highlight-current-row
+          max-height="800"
+        >
+          <el-table-column label="从" width="350" align="center">
+            <template slot-scope="scope">
+              <span>{{ scope.row.from }}</span>
+            </template>
+          </el-table-column>
+          <el-table-column label="到" width="350" align="center">
+            <template slot-scope="scope">
+              <span>{{ scope.row.to }}</span>
+            </template>
+          </el-table-column>
+          <el-table-column align="center" label="智能合约" width="150">
+            <template slot-scope="scope">
+              <span>{{ scope.row.contract }}</span>
+            </template>
+          </el-table-column>
+          <el-table-column align="center" prop="created_at" label="交易时间" width="200">
+            <template slot-scope="scope">
+              <span>{{ getTime(scope.row.timestamp) }}</span>
+            </template>
+          </el-table-column>
+        </el-table>
+      </el-tab-pane>
+      <el-tab-pane label="调用合约" name="third">
+        <el-table
+          v-loading="listLoading"
+          :data="invokeTrans"
+          element-loading-text="Loading"
+          border
+          highlight-current-row
+          max-height="800"
+        >
+          <el-table-column label="发起地址" width="350" align="center">
+            <template slot-scope="scope">
+              <span>{{ scope.row.from }}</span>
+            </template>
+          </el-table-column>
+<!--          <el-table-column label="到" width="350" align="center">-->
+<!--            <template slot-scope="scope">-->
+<!--              <span>{{ scope.row.to }}</span>-->
+<!--            </template>-->
+<!--          </el-table-column>-->
+          <el-table-column align="center" label="智能合约" width="150">
+            <template slot-scope="scope">
+              <span>{{ scope.row.contract }}</span>
+            </template>
+          </el-table-column>
+          <el-table-column align="center" label="方法" width="150">
+            <template v-if="scope.row.contract !== ''" slot-scope="scope">
+              <span>{{ scope.row.method }}</span>
+            </template>
+          </el-table-column>
+          <el-table-column align="center" label="参数" width="200">
+            <template v-if="scope.row.contract !== ''" slot-scope="scope">
+              <span>{{ scope.row.args }}</span>
+            </template>
+          </el-table-column>
+          <el-table-column align="center" prop="created_at" label="交易时间" width="200">
+            <template slot-scope="scope">
+              <span>{{ getTime(scope.row.timestamp) }}</span>
+            </template>
+          </el-table-column>
+        </el-table>
+      </el-tab-pane>
+    </el-tabs>
   </div>
 </template>
 
@@ -58,8 +115,11 @@ import { getAllTrans } from '@/api/ssbc'
 export default {
   data() {
     return {
-      list: null,
-      listLoading: true
+      transferTrans: [],
+      postTrans: [],
+      invokeTrans: [],
+      listLoading: true,
+      activeName: 'first'
     }
   },
   created() {
@@ -68,13 +128,32 @@ export default {
   methods: {
     getAllTrans() {
       getAllTrans().then(res => {
-        console.log('getAllTrans:', res)
-        this.list = res.Data
+        // 筛选出转账交易、发布合约和调用合约
+        const transfer = []
+        const post = []
+        const invoke = []
+        const total = res.Data
+        total.forEach(function(r) {
+          if (r.contract === '') {
+            transfer.push(r)
+          } else if (r.method === '') {
+            post.push(r)
+          } else {
+            invoke.push(r)
+          }
+        })
+        this.transferTrans = transfer
+        this.postTrans = post
+        this.invokeTrans = invoke
+
         this.listLoading = false
       })
     },
     getTime(str) {
       return str.slice(0, 19)
+    },
+    handleClick(tab, event) {
+      console.log(tab, event)
     }
   }
 }
